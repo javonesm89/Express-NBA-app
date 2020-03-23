f97beabe980867264ecd9b5e13c6de183c3c92b6
Javone <javosmith12@gmail.com>
2020-03-23 17:27:08 -0400

    Update nba.js

--- nba.js
+++ nba.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.get("/",function(req,res){
+    res.send("Working on NBA page!")
+})
+
 app.listen(3000,function(){
     console.log("running on 3000.")
 })
